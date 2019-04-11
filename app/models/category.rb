bee1dbbe83f069a699e675b9c99e92e7c959187d
3isawa <misawa.knct@gmail.com>
2019-04-11 23:33:05 +0900

    :sparkles: Categoryモデル及びcategoriesテーブルを作成

---
+++ app/models/category.rb
@@ -0,0 +1,3 @@
+class Category < ApplicationRecord
+  validetes :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_085543) do
+ActiveRecord::Schema.define(version: 2019_04_11_143124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2019_04_11_085543) do
     t.text "description"
     t.string "image"
     t.integer "borrowed_num", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
