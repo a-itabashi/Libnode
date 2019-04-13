56107e968c47b16a00cee7cb947d00554d46a6b0
3isawa <misawa.knct@gmail.com>
2019-04-13 15:12:27 +0900

    :sparkles: book_places, placesとのアソシエーションを設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,8 @@ class Book < ApplicationRecord
   has_many :book_categories
   has_many :categories, through: :book_categories
 
+  has_many :book_places
+  has_many :places, through: :book_places
+
   validates :title, presence: true
 end
