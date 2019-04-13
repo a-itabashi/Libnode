a2fbd0330d4272e166e7bdb915aef83835ec8756
3isawa <misawa.knct@gmail.com>
2019-04-13 15:14:01 +0900

    :sparkles: books、book_placesとのアソシエーションを設定

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   DEFAULT_NUMBER_OF_DIGITS = 2.freeze
 
+  has_many :book_places, dependent: :destroy
+  has_many :books, through: :book_places
+
   validates :shelf, :column, :row, presence: true, length: { maximum: DEFAULT_NUMBER_OF_DIGITS }
 end
