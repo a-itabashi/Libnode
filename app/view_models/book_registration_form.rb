e55d66f29062088e0930b38d3db6cffd7f8a75d6
3isawa <misawa.knct@gmail.com>
2019-04-14 11:03:43 +0900

    :tada: books、categories、placesの一括登録を可能にした

---
+++ app/view_models/book_registration_form.rb
@@ -0,0 +1,64 @@
+class BookRegistrationForm
+  include ActiveModel::Model
+
+  concerning :BookBuilder do
+    def book
+      @book ||= Book.new
+    end
+  end
+
+  concerning :CategoriesBuilder do
+    attr_reader :categories_attributes
+
+    def categories
+      @cateogries_attributes ||= Category.new
+    end
+
+    def categories_attributes=(attributes)
+      @cateogries_attributes = Category.new(attributes)
+    end
+  end
+
+  concerning :PlacesBuilder do
+    attr_reader :places_attributes
+
+    def places
+      @places_attributes ||= Place.new
+    end
+
+    def places_attributes=(attributes)
+      @places_attributes = Place.new(attributes)
+    end
+  end
+
+  attr_accessor :title, :author, :saled_at, :price, :description, :image
+
+  def save
+    book.assign_attributes(book_params)
+    build_informations
+
+    if book.save
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def book_params
+    {
+      title: title,
+      author: author,
+      saled_at: saled_at,
+      price: price,
+      description: description,
+      image: image
+    }
+  end
+
+  def build_informations
+    book.categories << categories
+    book.places << places
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :books, only: %i[index show]
 
   namespace :admin do
-    resources :books, only: %i[new edit create update destroy]
+    resources :registrations, only: %i[new edit create]
   end
 end
