b3379e2b91b6d952ca61a0642807a920aac90ea5
3isawa <misawa.knct@gmail.com>
2019-04-14 11:24:36 +0900

    :shirt: robocopに違反していた箇所を修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ApplicationRecord
-  has_many :book_categories
+  has_many :book_categories, dependent: :destroy
   has_many :categories, through: :book_categories
 
-  has_many :book_places
+  has_many :book_places, dependent: :destroy
   has_many :places, through: :book_places
 
   validates :title, presence: true
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ApplicationRecord
-  DEFAULT_NUMBER_OF_DIGITS = 2.freeze
+  DEFAULT_NUMBER_OF_DIGITS = 2
 
   has_many :book_places, dependent: :destroy
   has_many :books, through: :book_places
