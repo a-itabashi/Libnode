be5c03dcae94c4e6dd2ab468efede021623163dc
3isawa <misawa.knct@gmail.com>
2019-04-16 11:26:49 +0900

    :+1: BorrowListモデルのバリデーションを設定

--- app/models/borrow_list.rb
+++ app/models/borrow_list.rb
@@ -3,4 +3,5 @@ class BorrowList < ApplicationRecord
   belongs_to :user
 
   validates :book_id, uniqueness: { scope: :user_id }
+  validates :book_id, :user_id, :return_date, :is_return, presence: true
 end
