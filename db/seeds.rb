8982cae942e6d0edf2dbae5c11d99b8daa41292a
3isawa <misawa.knct@gmail.com>
2019-04-17 21:34:33 +0900

    :+1: adminカラムの制限を解除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'factory_bot_rails'
+
+FactoryBot.define do
+  factory :user do
+    name { "勤勉なエンジニアA" }
+  end
+
+  factory :portfolio do
+    explanation { "xxx" }
+  end
+
+  factory :comment do
+    content { "Awesome!" }
+  end
+
+  factory :correction do
+    sentence { "Not Good" }
+  end
+end
+
+user_a = FactoryBot.create(:user)
+portfolio_a = FactoryBot.create(:portfolio, user: user_a)
+
+3.times do |n|
+  FactoryBot.create(:comment, content: "content#{n}", user: user_a, portfolio: portfolio_a)
+  FactoryBot.create(:correction, sentence: "content#{n}", user: user_a, portfolio: portfolio_a)
+end
