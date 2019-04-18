858738be2fc0d042c09f5efdd4989d1a4aa60262
3isawa <misawa.knct@gmail.com>
2019-04-18 10:39:28 +0900

    :sparkles: BorrowListsコントローラーを作成

---
+++ app/controllers/borrow_lists_controller.rb
@@ -0,0 +1,13 @@
+class BorrowListsController < ApplicationController
+  def create
+    @borrow_list = BookList.new(borrow_list_params)
+
+    @borrow_list.save
+  end
+
+  private
+
+  def borrow_list_params
+    params.require(:borrow_list).permit(%i[book_id return_date])
+  end
+end
