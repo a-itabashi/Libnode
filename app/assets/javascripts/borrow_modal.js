1990864e49762c5b7bf8631fa6cc4e5214c58457
3isawa <misawa.knct@gmail.com>
2019-04-18 11:25:04 +0900

    :sparkles: 貸出ボタン押下と同時にモーダルが消えるようにした

--- app/assets/javascripts/borrow_modal.js
+++ app/assets/javascripts/borrow_modal.js
@@ -1,5 +1,9 @@
 $(function() {
-  $(".borrow-button").click(function(e) {
+  $(".borrow-button").click(function() {
     $('#borrow-modal').modal('show');
   });
+
+  $("#borrow-list-submit-button").click(function() {
+    $('#borrow-modal').modal('hide');
+  })
 })
