5a19846771d3525b78e0a27204b226f61193a496
3isawa <misawa.knct@gmail.com>
2019-04-20 18:35:22 +0900

    :sparkles: seedファイルを現実に沿った固

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,8 +51,33 @@ category_a = FactoryBot.create(:category)
 category_b = FactoryBot.create(:category, name: "カテゴリB")
 place_a = FactoryBot.create(:place)
 
-10. times do |n|
-  book_a = FactoryBot.create(:book, title: "タイトル#{n}")
+titles = [
+  "プロを目指す人のためのRuby入門",
+  "現場で使える Ruby on Rails 5速習実践ガイド",
+  "マスタリングTCP／IP（入門編）第5版",
+  "Vue．js入門",
+  "基礎から学ぶVue．js",
+  "SQL 第2版 ゼロからはじめるデータベース操作",
+  "わかばちゃんと学ぶGit使い方入門",
+  "オブジェクト指向設計実践ガイド",
+  "Python機械学習プログラミング第2版",
+  "アルゴリズム図鑑 絵で見てわかる26のアルゴリズム"
+]
+
+images = [
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/3977/9784774193977.jpg?_ex=200x200",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/2227/9784839962227.jpg?_ex=200x200",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/8768/9784274068768.jpg?_ex=200x200",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/0919/9784297100919.jpg?_ex=200x200",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/2457/9784863542457.jpg?_ex=200x200",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/4450/9784798144450.jpg?_ex=200x200",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/2174/9784863542174.jpg?_ex=200x200",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/3619/9784774183619.jpg?_ex=200x200",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/3373/9784295003373.jpg?_ex=200x200",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/9776/9784798149776.jpg?_ex=200x200"
+]
+10.times do |n|
+  book_a = FactoryBot.create(:book, title: titles[n], image: images[n])
   FactoryBot.create(:book_category, book: book_a, category: category_a)
   FactoryBot.create(:book_category, book: book_a, category: category_b)
   FactoryBot.create(:book_place, book: book_a, place: place_a)
