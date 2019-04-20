d1b8c8e412425c6d8acf47331783cfdab7451d85
3isawa <misawa.knct@gmail.com>
2019-04-20 20:15:26 +0900

    :recycle: 不要な機能の削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,4 @@
 class BooksController < ApplicationController
-  def index
-    @books = Book.all.borrow_lists.where(is_return: false)
-  end
-
   def show
     @book = Book.find(params[:id])
     render json: @book
