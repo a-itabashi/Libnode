808c1374d2476a688e86ba5d3a40bf77dfe4efaf
3isawa <misawa.knct@gmail.com>
2019-04-21 16:52:55 +0900

    :sparkles: サイドバーに蔵書編集画面へのリンクを作成

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,19 +1,19 @@
 class Admin::BooksController < Admin::ApplicationController
-  before_action :set_task, only: [:update, :destroy]
+  # before_action :set_book, only: [:update, :destroy]
 
   def edit
     @books = Book.all
     render 'book_collection_list/book_collection_list'
   end
 
-  def update;end
-  def destroy;end
+  # def update;end
+  # def destroy;end
 
-  private
+  # private
 
-  def set_book
-    @book = Book.find(params[:id])
-  end
+  # def set_book
+  #   @book = Book.find(params[:id])
+  # end
 
   # def book_params
   #   params.require(:book).permit(:)
