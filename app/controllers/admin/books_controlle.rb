79a7edf6a3272387b9234ed3ac4d39a79c886ab0
3isawa <misawa.knct@gmail.com>
2019-04-21 16:41:29 +0900

    :sparkles: admin:booksコントローラを作成

---
+++ app/controllers/admin/books_controlle.rb
@@ -0,0 +1,21 @@
+class Admin::BooksController < Admin::ApplicationController
+  before_action :set_task, only: [:update, :destroy]
+
+  def edit
+    @books = Book.all
+    render 'book_collection_list/book_collection_list'
+  end
+
+  def update;end
+  def destroy;end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  # def book_params
+  #   params.require(:book).permit(:)
+  # end
+end
