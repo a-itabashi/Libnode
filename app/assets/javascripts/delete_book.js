d9ea7d51e596ebffc6018d2d3f9a679169fb88c4
3isawa <misawa.knct@gmail.com>
2019-04-22 11:16:55 +0900

    :sparkles: 書籍の削除機能を実装

---
+++ app/assets/javascripts/delete_book.js
@@ -0,0 +1,17 @@
+$(function () {
+  $(".book-delete").click(function(e) {
+    if (e) e.preventDefault();
+
+    let set_id = $(this).attr('id')
+      $.ajax({
+        // TODO: ドメイン名を変更
+        url: `http://localhost:3000/admin/books/${set_id}`,
+        type: 'delete',
+        dataType: 'json',
+      }).done(function(response) {
+        // TODO: ドメイン名を変更
+        window.location.href = "http://localhost:3000/admin/book_collection_list/edit";
+      }).fail(function() {
+    });
+  });
+});
