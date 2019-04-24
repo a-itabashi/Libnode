b213b6f73a9b6fd070b6ed972611384465c3daf2
3isawa <misawa.knct@gmail.com>
2019-04-24 23:27:52 +0900

    :green_heart: モデルスペックを一部書いた

---
+++ spec/factories/book_place.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :book_place do
+    book_id { 1 }
+    place_id { 1 }
+  end
+end
---
+++ spec/models/book_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  it "titleが空ならバリデーションが通らない" do
+    book = FactoryBot.build(:book, title: "")
+    book.valid?
+    expect(book.errors[:title]).to include("can't be blank")
+  end
+end
