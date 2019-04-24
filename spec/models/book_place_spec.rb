96bd4f18180e505e18f3cbe9f8ded1899c4462de
3isawa <misawa.knct@gmail.com>
2019-04-24 23:35:39 +0900

    :green_heart: CircleCIテスト

--- spec/models/book_place_spec.rb
+++ spec/models/book_place_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe BookPlace, type: :model do
   it '同じ本に複数のplaceが紐付かない' do
     book_place_1 = FactoryBot.build(:book_place)
     book_place_2 = FactoryBot.build(:book_place, place_id: 2)
-    expect(book_place_2).not_to be_valid
+    expect(book_place_2).not_to be_invalid
   end
 end
