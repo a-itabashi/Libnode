b151a10e9a4edec5862b00749b8cf2f1547e8e10
3isawa <misawa.knct@gmail.com>
2019-04-24 16:07:30 +0900

    :sparkles: 管理者への変更、ユーザーの削除を可能にした

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,20 @@
 class Admin::UsersController < Admin::ApplicationController
+  before_action :set_user, only: %i[update destroy]
   def index
     @users = User.all
+  end
+
+  def update
+    redirect_to admin_users_path, success: "#{@user.name}さんを管理者ユーザーに変更しました" if @user.update(admin: true)
+  end
+
+  def destroy
+    redirect_to admin_users_path, success: "#{@user.name}さんを削除しました" if @user.destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
