ba182355d826f0905d91fe7a6d026fb97c702b1e
3isawa <misawa.knct@gmail.com>
2019-04-24 13:04:14 +0900

    :shirt: rubocopに指摘された箇所を修正

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,8 +6,6 @@ class Admin::ApplicationController < ApplicationController
 
   def admin?
     authenticate_user! # TODO: 将来的に削除
-    unless current_user.nil? || current_user.admin == true
-      redirect_to root_path, danger: "管理者のみ実行可能なアクションです"
-    end
+    redirect_to root_path, danger: '管理者のみ実行可能なアクションです' unless current_user.nil? || current_user.admin == true
   end
 end
