a064509f4fbebaa09504b6aa2920c5044ad09e30
3isawa <misawa.knct@gmail.com>
2019-04-26 12:04:44 +0900

    :sparkles: asset precompileに関する記述を追記

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Libnode
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.initialize_on_precompile = false
     config.load_defaults 5.2
     config.generators.template_engine = :slim
     config.time_zone = 'Tokyo'
