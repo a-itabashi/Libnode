2014ea8e3874002d4ebc02714f16806b56f568e9
3isawa <misawa.knct@gmail.com>
2019-04-26 23:46:28 +0900

    :sparkles: sidekiqの設定(途中...)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,5 @@
+schedule_file = "config/schedule.yml"
+
 Sidekiq.configure_server do |config|
   case Rails.env
     when 'production' then
@@ -19,8 +21,6 @@ Sidekiq.configure_client do |config|
       config.redis = { url: 'redis://127.0.0.1:6379', namespace: 'sidekiq' }
   end
 end
-
-schedule_file = "config/schedule.yml"
 
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
