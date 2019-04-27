991454b46ae2ed004426a3fae9ac7f105dac17df
3isawa <misawa.knct@gmail.com>
2019-04-28 00:57:44 +0900

    :sparkles: 書籍登録時の成功・失敗メッセージを表示

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -9,8 +9,9 @@ class Admin::RegistrationsController < Admin::ApplicationController
     @book_registrations_form = BookRegistrationForm.new(book_registration_form_params)
 
     if @book_registrations_form.save
-      redirect_to new_admin_registration_path
+      redirect_to new_admin_registration_path, success: '書籍の作成が成功しました'
     else
+      flash.now[:danger] = '書籍の作成に失敗しました'
       render :new
     end
   end
