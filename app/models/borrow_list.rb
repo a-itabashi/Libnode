35407539b51069808b014631c99e12334fa94556
3isawa <misawa.knct@gmail.com>
2019-04-27 19:18:14 +0900

    :+1: 貸出の際、返却期限の過去日を入力可能にした

--- app/models/borrow_list.rb
+++ app/models/borrow_list.rb
@@ -4,8 +4,15 @@ class BorrowList < ApplicationRecord
 
   validates :book_id, uniqueness: { scope: :user_id }, on: :create, unless: :all_books_returned?
   validates :book_id, :user_id, :return_date, presence: true
+  validate :return_date_must_be_future_date
+
+  private
 
   def all_books_returned?
     BorrowList.where(book_id: book_id).map(&:is_return).all?
+  end
+
+  def return_date_must_be_future_date
+    errors.add(:return_date, '過去の日付は表示できません') if return_date <= Time.zone.today
   end
 end
