fe287ee0cade9bb44858fc2632eba58288869b99
3isawa <misawa.knct@gmail.com>
2019-04-29 13:09:16 +0900

    :+1: 貸出時のバリデーションエラーメッセージを修正

--- app/models/borrow_list.rb
+++ app/models/borrow_list.rb
@@ -3,9 +3,8 @@ class BorrowList < ApplicationRecord
   belongs_to :user
 
   validates :book_id, uniqueness: { scope: :user_id }, on: :create, unless: :all_books_returned?
-  validates :book_id, :user_id, :return_date, presence: true
+  # validates :book_id, :user_id, :return_date, presence: true
   validate :return_date_must_be_future_date
-  # validate :book_id_must_not_blank
 
   private
 
@@ -14,14 +13,8 @@ class BorrowList < ApplicationRecord
   end
 
   def return_date_must_be_future_date
-    if return_date.nil?
-      errors.add(:error, '返却期限を入力して下さい')
-    elsif return_date <= Time.zone.today
-      errors.add(:error, '過去の日付は表示できません')
+    unless return_date.nil?
+      errors.add(:error, '過去の日付は表示できません') if return_date <= Time.zone.today
     end
-  end
-
-  def book_id_must_not_blank
-    errors.add(:error, '書籍番号は必ず入力して下さい') if book_id.nil?
   end
 end
