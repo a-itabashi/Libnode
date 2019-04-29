affdeec3a70bdd6dc2e6b2208cf91a8ef7061f0b
3isawa <misawa.knct@gmail.com>
2019-04-29 12:38:17 +0900

    :+1: メール送信メソッドをsmtpに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
 
   # config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'libnode.herokuapp.com' }
-  config.action_mailer.delivery_method = :letter_opener_web
-  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
