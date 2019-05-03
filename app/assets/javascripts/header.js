6f8e8db9747378ce8c16032db4c3fce554fb2755
3isawa <misawa.knct@gmail.com>
2019-05-03 18:07:47 +0900

    :sparkles: ヘッダーに検索解除ボタンを作成

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -3,11 +3,4 @@ $ (function () {
     console.log("change");
     $(".search-btn").click();
   });
-  $("#q_categories_id_eq").hover(function () {
-    $(this).click();
-  });
-  $(".category-select").hover(function () {
-    console.log("i")
-    $("select [name=q[categories_id_eq]]").click();
-  });
 })
