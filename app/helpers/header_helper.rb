8d26a68d5299b59a8d283ece48dd5882b1354730
3isawa <misawa.knct@gmail.com>
2019-05-09 22:54:32 +0900

    :+1: ヘッダーをパーシャル化

--- app/helpers/header_helper.rb
+++ app/helpers/header_helper.rb
@@ -2,9 +2,27 @@ module HeaderHelper
   def header_selector
     if controller.controller_name == 'books' && controller.action_name == 'index' \
       || controller.controller_name == 'books' && controller.action_name == 'edit'
-      'header_with_searchbox'
-    else
-      'header'
+      render(partial: 'layouts/header/header_with_searchbox')
+
+    elsif controller.controller_name == 'trends'
+      render(partial: 'layouts/header/header',
+             locals: { icon_type: 'fas',
+                       icon: 'angle-double-up',
+                       title: 'トレンドページ',
+                       caption: 'Amazon技術書ランキングから旬の技術書をご紹介' })
+    elsif controller.controller_name == 'rankings'
+      render(partial: 'layouts/header/header',
+             locals: { icon_type: 'fas',
+                       icon: 'crown',
+                       title: 'ランキングページ',
+                       caption: '' })
+    elsif controller.controller_name == 'registrations' && controller.action_name == 'new' \
+      || controller.controller_name == 'import_csvs' && controller.action_name == 'new'
+      render(partial: 'layouts/header/header',
+             locals: { icon_type: '',
+                       icon: '',
+                       title: '蔵書登録ページ',
+                       caption: '書籍情報をLibnodeに新規登録！' })
     end
   end
 end
