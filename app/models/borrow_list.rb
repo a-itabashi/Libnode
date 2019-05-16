e1b88f6c943f48a6efb012997da56a346e345d03
3isawa <misawa.knct@gmail.com>
2019-05-16 20:30:47 +0900

    :shirt: rubocopに指摘された箇所を修正

--- app/models/borrow_list.rb
+++ app/models/borrow_list.rb
@@ -2,14 +2,13 @@ class BorrowList < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
-  validates :book_id, uniqueness: true, on: :create, unless: :all_books_returned?, uniqueness: { message: 'は既に借りられています' }
-  # validates :book_id, :user_id, :return_date, presence: true
+  validates :book_id, uniqueness: true, on: :create, unless: :all_returned?, uniqueness: { message: 'は既に借りられています' }
   validates :user_id, :return_date, presence: true
   validate :return_date_must_be_future_date
 
   private
 
-  def all_books_returned?
+  def all_returned?
     BorrowList.where(book_id: book_id).map(&:is_return).all?
   end
 
