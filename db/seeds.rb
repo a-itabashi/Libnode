d6bf695efa89e040997498c84b1fa0dafc7d3276
3isawa <misawa.knct@gmail.com>
2019-05-21 17:01:13 +0900

    :+1: seedデータを更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# require 'factory_bot_rails'
-
-FactoryBot.define do
-  factory :place do
-    shelf { 11 }
-    column { 22 }
-    row { 33 }
-  end
-end
-
 user_a = FactoryBot.create(:user)
 category_a = FactoryBot.create(:category, name: "カテゴリA")
 category_b = FactoryBot.create(:category, name: "カテゴリB")
