66076099ccfdffb28569f955704f65a47cfea224
3isawa <misawa.knct@gmail.com>
2019-05-24 22:19:06 +0900

    :bug: 書籍編集画面で書籍の検索が行えない問題を修正

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -2,8 +2,11 @@ class Admin::BooksController < Admin::ApplicationController
   before_action :set_book, only: %i[destroy]
 
   def edit
-    @books = Book.all
+    # @books = Book.all
+    @search = Book.ransack(params[:q])
+    @books = @search.result
     render 'books/index'
+
   end
 
   def destroy
