44f674c4faa2d3f8c16e45f365514823e4491c40
3isawa <misawa.knct@gmail.com>
2019-05-26 13:27:51 +0900

    re

--- app/assets/javascripts/trends/purchase_request.js
+++ app/assets/javascripts/trends/purchase_request.js
@@ -5,11 +5,11 @@ $(function () {
 
   $("#purchase-request-button").click(function(e) {
     $("#purchase-request-modal").modal("show", e.target);
-    history.pushState(null, null, "#purchase-request-modal");
+    window.history.pushState(null, null, "#purchase-request-modal");
     $("#request-book-title").val(e.target.parentElement.children[1].textContent);
   });
 
   $("#purchase-request-modal").on("hide.bs.modal", function () {
-    history.replaceState(null,null,"/");
+    window.history.replaceState(null,null,"/");
   });
 });
